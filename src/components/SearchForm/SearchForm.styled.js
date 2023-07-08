3e02fef4a2b71f76ecba7d5db1fda3e597a7eb57
Cosmits <salge.cos@gmail.com>
2023-07-08 21:55:37 +0200

    fix style

--- src/components/SearchForm/SearchForm.styled.js
+++ src/components/SearchForm/SearchForm.styled.js
@@ -4,6 +4,7 @@ export const StyledForm = styled.form`
   padding: 10px 0 20px 0;
   display: flex;
   align-items: baseline;
+  justify-content: center;
 `
 
 export const StyledInput = styled.input`
@@ -11,11 +12,12 @@ export const StyledInput = styled.input`
   border-bottom: 1px solid #171212;
   background-color: transparent;
   font-size: 24px;
+  margin: 0 24px
 `
 
 export const StyledLabel = styled.label`
-  border-top: 1px solid #171212;
-  border-left: 1px solid #171212;
-  padding-left: 5px;
+  border: 1px solid #171212;
+  border-radius: 8px;
+  padding: 0 8px;
   font-size: 24px;
 `
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,5 @@ import './index.css'
 import App from './components/App/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <App />
-  </>,
+  <App />,
 )
