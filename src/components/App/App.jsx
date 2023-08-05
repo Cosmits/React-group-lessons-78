bf818ef69025f0351a2b6b7f71a4e0597fe466ee
Cosmits <salge.cos@gmail.com>
2023-08-05 11:20:59 +0200

    fix App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,62 +1,13 @@
-// import { Component } from 'react';
-// import { nanoid } from 'nanoid';
-
 import {
   Container,
-  // Grid,
-  // GridItem,
   Header,
   SearchForm,
   Section,
-  // Text,
-  // Todo,
 } from 'components';
 import TodoList from 'components/TodoList/TodoList';
 
 export const App = () => {
-  // state = {
-  //   todos: [],
-  // };
-
-  // componentDidMount() {
-  //   const todos = JSON.parse(localStorage.getItem('todos'));
-
-  //   if (todos) {
-  //     this.setState(() => ({ todos }));
-  //   }
-  // }
-  // componentDidUpdate(prevProps, prevState) {
-  //   const { todos } = this.state;
-
-  //   if (prevState.todos !== todos) {
-  //     localStorage.setItem('todos', JSON.stringify(todos));
-  //   }
-  // }
-
-  // addTodo = text => {
-  //   const todo = {
-  //     id: nanoid(),
-  //     text,
-  //   };
-
-  //   this.setState(({ todos }) => ({
-  //     todos: [...todos, todo],
-  //   }));
-  // };
-
-  // handleSubmit = data => {
-  //   this.addTodo(data);
-  // };
-
-  // deleteTodo = id => {
-  //   this.setState(prevState => ({
-  //     todos: prevState.todos.filter(todo => todo.id !== id),
-  //   }));
-  // };
-  // render() {
-  //   const { todos } = this.state;
-
-  return (
+   return (
     <>
       <Header />
       <Section>
@@ -67,5 +18,4 @@ export const App = () => {
       </Section>
     </>
   );
-
 }
